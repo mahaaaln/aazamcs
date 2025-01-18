a6fb3283ac5d8b14513780014dc3e5179b275170
AlMaha <aalnassr@andrew.cmu.edu>
2025-01-18 14:39:08 +0300

    Initial Commit

---
+++ Stopwatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Maha Alnassr on 18/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StopwatchTests/StopwatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchTests.swift
+//  StopwatchTests
+//
+//  Created by Maha Alnassr on 18/01/2025.
+//
+
+import Testing
+@testable import Stopwatch
+
+struct StopwatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
